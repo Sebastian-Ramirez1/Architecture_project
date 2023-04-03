3e27a2ceb9dc4800c03216630fa7b024d69d39f7
IgnacioRiveraPrieto <ignacio.rivera1@upr.edu>
2023-04-03 14:42:27 -0400

    Added files for Phase 3 implementation

---
+++ PF3.v
@@ -0,0 +1,341 @@
+module ControlUnit(Instr, ID_OP, ID_jmpl_instr, ID_Read_Write, ID_SE_dm, ID_load_instr, ID_RF_enable, ID_size_dm, ID_modifyCC, ID_Call_instr, ID_B_instr, ID_29_a, ID_ALU_op3);
+    input [31:0] Instr;
+    output reg ID_jmpl_instr, ID_Read_Write, ID_SE_dm, ID_load_instr, ID_RF_enable, ID_modifyCC, ID_Call_instr, ID_B_instr, ID_29_a;
+    output reg [1:0] ID_size_dm;
+    output reg [1:0] ID_OP;
+    output reg [5:0] ID_ALU_op3;
+
+    wire [1:0] op = Instr[31:30];
+
+    always @(*) 
+        begin
+
+            case (op)
+            2'b01:
+            begin
+                //op = CALL
+                ID_OP = op;
+                ID_jmpl_instr = 0;
+                ID_Read_Write = 'X;
+                ID_SE_dm = 'X;
+                ID_load_instr = 0;
+                ID_RF_enable = 1; //Write in R15 PC
+                ID_size_dm = 'X;
+                ID_modifyCC = 'X;
+                ID_Call_instr = 1;
+                ID_B_instr = 0;
+                ID_29_a = 'X;
+                ID_ALU_op3 = 'X;
+            end
+    
+            2'b00:
+            begin
+                //op = Branch
+                ID_OP = op;
+                ID_jmpl_instr = 0;
+                ID_Read_Write = 0;
+                ID_SE_dm = 0;
+                ID_load_instr = 0;
+                ID_RF_enable = 0;
+                ID_size_dm = 0;
+                ID_modifyCC = 0;
+                ID_Call_instr = 0;
+                ID_B_instr = 1;
+                ID_29_a = Instr[29];
+                ID_ALU_op3 = 000000;
+            end
+
+            2'b10:
+            begin
+                //op = Arithmetic or Jmpl
+
+                if(Instr[24:19] == 6'b111000) begin
+                    ID_jmpl_instr = 1;
+                    ID_modifyCC = 0;
+                end else if ( (Instr[24:19] == 6'b010000) || (Instr[24:19] == 6'b011000) || (Instr[24:19] == 6'b010100) || (Instr[24:19] == 6'b011100)) begin
+                    ID_modifyCC = 1;
+                    ID_jmpl_instr = 0;
+                end
+
+                ID_OP = op;
+                ID_Read_Write = 'X;
+                ID_SE_dm = 'X;
+                ID_load_instr = 0;
+                ID_RF_enable = 1;
+                ID_size_dm = 'X;
+                ID_Call_instr = 0;
+                ID_B_instr = 0;
+                ID_29_a = 'X;
+                ID_ALU_op3 = Instr[24:19];
+            end
+
+            2'b11:
+            begin
+                //op = Load/Store
+                case (Instr[24:19])
+                6'b001001: //load sign byte
+                begin
+                    ID_Read_Write = 0; //Load = 0
+                    ID_SE_dm = 1; //Signed Extension
+                    ID_load_instr = 1; //Enable
+                    ID_RF_enable = 1; //Ubicar en Rd un valor de memoria 
+                    ID_size_dm = 00; //byte
+                end
+                6'b001010: //load sign halfword
+                begin
+                    ID_Read_Write = 0; //Load = 0
+                    ID_SE_dm = 1; //Signed Extension
+                    ID_load_instr = 1; //Enable
+                    ID_RF_enable = 1; //Ubicar en Rd un valor de memoria 
+                    ID_size_dm = 01; //halfword
+                end
+                6'b000000: //load word
+                begin
+                    ID_Read_Write = 0; //Load = 0
+                    ID_SE_dm = 'X; //Signed Extension
+                    ID_load_instr = 1; //Enable
+                    ID_RF_enable = 1; //Ubicar en Rd un valor de memoria 
+                    ID_size_dm = 10; //word
+                end
+                6'b000001: //load unsigned byte
+                begin
+                    ID_Read_Write = 0; //Load = 0
+                    ID_SE_dm = 0; //Signed Extension
+                    ID_load_instr = 1; //Enable
+                    ID_RF_enable = 1; //Ubicar en Rd un valor de memoria 
+                    ID_size_dm = 00; //byte
+                end
+                6'b000010: //load unsigned halfword
+                begin
+                    ID_Read_Write = 0; //Load = 0
+                    ID_SE_dm = 0; //Signed Extension
+                    ID_load_instr = 1; //Enable
+                    ID_RF_enable = 1; //Ubicar en Rd un valor de memoria 
+                    ID_size_dm = 01; //halfword
+                end
+                6'b000101: //store byte
+                begin
+                    ID_Read_Write = 1; //Store = 1
+                    ID_SE_dm = 'X; //Signed Extension
+                    ID_load_instr = 1; //Enable
+                    ID_RF_enable = 0; //Ubicar en Memoria solamente 
+                    ID_size_dm = 00; //byte
+                end
+                6'b000110: //store halfword
+                begin
+                    ID_Read_Write = 1; //Store = 1
+                    ID_SE_dm = 'X; //Signed Extension
+                    ID_load_instr = 1; //Enable
+                    ID_RF_enable = 0; //Ubicar en Memoria solamente 
+                    ID_size_dm = 01; //byte
+                end
+                6'b000100: //store word
+                begin
+                    ID_Read_Write = 1; //Store = 1
+                    ID_SE_dm = 'X; //Signed Extension
+                    ID_load_instr = 1; //Enable
+                    ID_RF_enable = 0; //Ubicar en Memoria solamente 
+                    ID_size_dm = 10; //Word
+                end
+                endcase
+                ID_OP = op;
+                ID_jmpl_instr = 0;
+                ID_modifyCC = 0;
+                ID_Call_instr = 0;
+                ID_B_instr = 0;
+                ID_29_a = 'X;
+                ID_ALU_op3 =Instr[24:19];
+            end
+
+            endcase
+        
+        end
+
+
+
+endmodule
+
+module MuxControlSignal(S, ID_jmpl_instr, ID_Read_Write, ID_SE_dm, ID_load_instr, ID_RF_enable, ID_size_dm, ID_modifyCC, ID_Call_instr, ID_B_instr, ID_29_a, ID_ALU_op3, ID_jmpl_instr_out, ID_Read_Write_out, ID_SE_dm_out, ID_load_instr_out, ID_RF_enable_out, ID_size_dm_out, ID_modifyCC_out, ID_Call_instr_out, ID_B_instr_out, ID_29_a_out, ID_ALU_op3_out);
+    input S;
+    input ID_jmpl_instr, ID_Read_Write, ID_SE_dm, ID_load_instr, ID_RF_enable, ID_modifyCC, ID_Call_instr, ID_B_instr, ID_29_a;
+    input [1:0] ID_size_dm; 
+    input [5:0] ID_ALU_op3;
+    output reg ID_jmpl_instr_out, ID_Read_Write_out, ID_SE_dm_out, ID_load_instr_out, ID_RF_enable_out, ID_modifyCC_out, ID_Call_instr_out, ID_B_instr_out, ID_29_a_out;
+    output reg [1:0] ID_size_dm_out;
+    output reg [5:0] ID_ALU_op3_out;
+
+    always @(*) 
+        begin
+        case (S)
+            1'b0: // Buffer
+            begin
+                ID_jmpl_instr_out = ID_jmpl_instr;
+                ID_Read_Write_out = ID_Read_Write;
+                ID_SE_dm_out = ID_SE_dm;
+                ID_load_instr_out = ID_load_instr;
+                ID_RF_enable_out = ID_RF_enable;
+                ID_size_dm_out = ID_size_dm;
+                ID_modifyCC_out = ID_modifyCC;
+                ID_Call_instr_out = ID_Call_instr;
+                ID_B_instr_out = ID_B_instr;
+                ID_29_a_out = ID_29_a;
+                ID_ALU_op3_out = ID_ALU_op3;
+            end
+                
+            1'b1: //No Operation
+            begin
+                ID_jmpl_instr_out = 1'b0;
+                ID_Read_Write_out = 1'b0;
+                ID_SE_dm_out = 1'b0;
+                ID_load_instr_out = 1'b0;
+                ID_RF_enable_out = 1'b0;
+                ID_size_dm_out = 2'b00;
+                ID_modifyCC_out = 1'b0;
+                ID_Call_instr_out = 1'b0;
+                ID_B_instr_out = 1'b0;
+                ID_29_a_out = 1'b0;
+                ID_ALU_op3_out = 6'b000000;    
+            end
+                
+        endcase
+
+        end
+
+    
+endmodule
+
+module Sumador4(PC, nPC); 
+    //input [31:0] PC;
+    //output reg [31:0] nPC;
+    input [7:0] PC;
+    output reg [7:0] nPC;
+    always @(PC, nPC) 
+        begin
+            //wire [31:0] sum = PC + 4;
+            nPC = PC + 4;
+        end
+    
+endmodule
+
+module nPC (Clk, D, Q, LE, R);
+   //input [31:0] D;
+    input [7:0] D;
+    input LE;
+    input Clk;
+    input R;
+    output reg [7:0] Q;
+    //output reg [31:0] Q;
+
+    always @(posedge Clk, R) //0 --> 1 en Clk: entra al if
+        begin
+            if (R) Q <= 8'b0000100; //En el caso de nPC un reset produce un número binario correspondiente a un 4.
+            else if (LE) Q <= D; // LE = 1  D --> Q
+        end
+
+endmodule
+
+module PC (Clk, D, Q, LE, R);
+    //input [31:0] D;
+    input [7:0] D;
+    input LE;
+    input Clk;
+    input R;
+    output reg [7:0] Q;
+    //output reg [31:0] Q;
+
+always @(posedge Clk, R) //0 --> 1 en Clk: entra al if
+    begin
+        if (R) Q <= 8'b00000000; //un reset tienen el efecto de hacer cero todos los bits de salida del registro. 
+        else if (LE) Q <= D; // LE = 1  D --> Q
+    end
+    
+endmodule
+
+module InstructionMemory (output reg [31:0] DataOut, input [7:0] Address);
+
+    reg [7:0] Mem [0:511];
+    always @(Address) begin
+    // Reading the Data
+        DataOut = {Mem[Address], Mem[Address+1], Mem[Address+2], Mem[Address+3]};
+    
+    end
+
+endmodule
+
+module PipelineRegister_IF_ID(Clk, Instr, Q, LE, R);
+    input [31:0] Instr;
+    input LE;
+    input Clk;
+    input R;
+    output reg [31:0] Q;
+
+always @(posedge Clk, R, LE) //0 --> 1 en Clk: entra al if
+    begin
+        if (R) Q <= 32'b00000000000000000000000000000000; //un reset tienen el efecto de hacer cero todos los bits de salida del registro. 
+        else if (LE) Q <= Instr; // LE = 1  D --> Q
+    end
+    
+endmodule
+
+module PipelineRegister_ID_EX(Clk, Instr, Q, EX_jmpl_instr, EX_Read_Write, EX_ALU_op3, EX_SE_dm, EX_load_instr, EX_RF_enable, EX_size_dm, EX_modifyCC, EX_jmpl_instr_out, EX_Read_Write_out, EX_ALU_op3_out, EX_SE_dm_out, EX_load_instr_out, EX_RF_enable_out, EX_size_dm_out, EX_modifyCC_out);
+    input [31:0] Instr;
+    input Clk;
+    input EX_jmpl_instr, EX_Read_Write, EX_SE_dm, EX_load_instr, EX_RF_enable , EX_modifyCC;
+    input [1:0] EX_size_dm;
+    input [5:0] EX_ALU_op3;
+    output reg EX_jmpl_instr_out, EX_Read_Write_out, EX_SE_dm_out, EX_load_instr_out, EX_RF_enable_out , EX_modifyCC_out;
+    output reg [1:0] EX_size_dm_out;
+    output reg [5:0] EX_ALU_op3_out;
+    output reg [31:0] Q;
+
+always @(posedge Clk) //0 --> 1 en Clk: entra al if
+    begin
+        Q <= Instr; //Output <= Input
+        EX_jmpl_instr_out <= EX_jmpl_instr;
+        EX_Read_Write_out <= EX_Read_Write;
+        EX_ALU_op3_out <= EX_ALU_op3; 
+        EX_SE_dm_out <= EX_SE_dm; 
+        EX_load_instr_out <= EX_load_instr; 
+        EX_RF_enable_out <= EX_RF_enable; 
+        EX_size_dm_out <= EX_size_dm; 
+        EX_modifyCC_out <= EX_modifyCC;
+    end
+    
+endmodule
+
+module PipelineRegister_EX_MEM(Clk, Instr, Q, MEM_jmpl_instr, MEM_Read_Write, MEM_SE_dm, MEM_load_instr, MEM_RF_enable, MEM_size_dm, MEM_jmpl_instr_out, MEM_Read_Write_out, MEM_SE_dm_out, MEM_load_instr_out, MEM_RF_enable_out, MEM_size_dm_out);
+    input [31:0] Instr;
+    input Clk;
+    input MEM_jmpl_instr, MEM_Read_Write, MEM_SE_dm, MEM_load_instr, MEM_RF_enable;
+    input [1:0] MEM_size_dm;
+    output reg MEM_jmpl_instr_out, MEM_Read_Write_out, MEM_SE_dm_out, MEM_load_instr_out, MEM_RF_enable_out;
+    output reg [1:0] MEM_size_dm_out;
+    output reg [31:0] Q;
+
+always @(posedge Clk) //0 --> 1 en Clk: entra al if
+    begin
+        Q <= Instr; //Output <= Input
+        MEM_jmpl_instr_out <= MEM_jmpl_instr;
+        MEM_Read_Write_out <= MEM_Read_Write;
+        MEM_SE_dm_out <= MEM_SE_dm; 
+        MEM_load_instr_out <= MEM_load_instr; 
+        MEM_RF_enable_out <= MEM_RF_enable; 
+        MEM_size_dm_out <= MEM_size_dm;
+    end
+    
+endmodule
+
+module PipelineRegister_MEM_WB(Clk, Instr, Q, WB_RF_enable, WB_RF_enable_out);
+    input [31:0] Instr;
+    input Clk;
+    input WB_RF_enable;
+    output reg WB_RF_enable_out;
+    output reg [31:0] Q;
+
+always @(posedge Clk) //0 --> 1 en Clk: entra al if
+    begin
+        Q <= Instr; //Output <= Input
+        WB_RF_enable_out <= WB_RF_enable; 
+    end
+    
+endmodule
---
+++ PF3_mp.v
@@ -0,0 +1,103 @@
+`include "PF3.v"
+
+module ControlUnitModuloPrueba;
+
+    //inputs and outputs for MuxControlSignal
+    reg S; //MUXControlSignal Signal
+    reg Clk; //Clock Signal 
+    reg LE; //Load Enable Signal
+    reg R; //Reset Signal
+
+    //Input and Output for Control Unit and MUXControlSignal
+    wire [5:0] ID_ALU_op3; //ALU_op3 input sigal
+    wire [1:0] ID_op;
+    wire [1:0] ID_size_dm;
+    wire ID_jmpl_instr, ID_Read_Write, ID_SE_dm, ID_load_instr, ID_RF_enable, ID_modifyCC, ID_Call_instr, ID_B_instr, ID_29_a; //output ControlUnit => input for MuxControlUNit
+    wire ID_jmpl_instr_out, ID_Read_Write_out, ID_SE_dm_out, ID_load_instr_out, ID_RF_enable_out, ID_modifyCC_out, ID_Call_instr_out, ID_B_instr_out, ID_29_a_out;
+    wire [1:0] ID_size_dm_out; 
+    wire [5:0] ID_ALU_op3_out;
+
+    //Output Control Signals for PipelineRegister_ID_EX
+    wire EX_jmpl_instr, EX_Read_Write, EX_SE_dm, EX_load_instr, EX_RF_enable, EX_modifyCC;
+    wire [1:0] EX_size_dm; 
+    wire [5:0] EX_ALU_op3;
+
+    //Output Control Signals for PipelineRegister_EX_MEM
+    wire MEM_jmpl_instr, MEM_Read_Write, MEM_SE_dm, MEM_load_instr, MEM_RF_enable;
+    wire [1:0] MEM_size_dm;
+    
+    //Output Control Signals for PipelineRegister_MEM_WB
+    wire WB_RF_enable;
+
+    wire [31:0] PipelineRegister_IF_ID_Out; //Output_Pipeline_Register_IF_ID => Input_ControlUnit and Input_PipelineRegister_ID_EX
+    wire [31:0] PipelineRegister_ID_EX_Out; //Output_Pipeline_Register_ID_EX => Input_PipelineRegister_EX_MEM
+    wire [31:0] PipelineRegister_EX_MEM_Out; //Output_Pipeline_Register_EX_MEM => Input_PipelineRegister_MEM_WB
+    wire [31:0] PipelineRegister_MEM_WB_Out; //Output_Pipeline_Register_MEM_WB => Input_PW_RegisterFile
+
+    integer fr, fw, code; //variables para leer archivo
+    reg [7:0] FileData; //variablele to store the data from file
+    reg [7:0] Address; //variable to indicate where to store in the instruction memory
+
+    reg [31:0]Instr; //INstruction to test Control Unit
+
+    wire [7:0] PC_In; //Output_nPC => Input_PC
+    wire [7:0] PC_Out; //Ouput_PC => Input_Sumador4 and Input_InstructionMemory
+    wire [7:0] Sumador4_Out; //Output Sumador4 => Input_nPC
+    wire [31:0] InstructionMemory_Out; //Output_InsturctionMemory => Input_PipeplineRegister_IF_ID
+
+    PC PC(Clk, PC_In, PC_Out, LE, R); //instancia de PC
+    Sumador4 Sumador4(PC_Out, Sumador4_Out); // instancia de Sumador de PC
+    nPC nPC(Clk, Sumador4_Out, PC_In, LE, R); // instancia de nPC
+    
+    InstructionMemory InstructionMemory(InstructionMemory_Out, PC_Out); //instancia de instruction memory
+
+    PipelineRegister_IF_ID PipelineRegister_IF_ID(Clk, InstructionMemory_Out, PipelineRegister_IF_ID_Out, LE, R);
+
+    ControlUnit ControlUnit(PipelineRegister_IF_ID_Out, ID_op, ID_jmpl_instr, ID_Read_Write, ID_SE_dm, ID_load_instr, ID_RF_enable, ID_size_dm, ID_modifyCC, ID_Call_instr, ID_B_instr, ID_29_a, ID_ALU_op3);
+    MuxControlSignal MuxControlSignal(S, ID_jmpl_instr, ID_Read_Write, ID_SE_dm, ID_load_instr, ID_RF_enable, ID_size_dm, ID_modifyCC, ID_Call_instr, ID_B_instr, ID_29_a, ID_ALU_op3, ID_jmpl_instr_out, ID_Read_Write_out, ID_SE_dm_out, ID_load_instr_out, ID_RF_enable_out, ID_size_dm_out, ID_modifyCC_out, ID_Call_instr_out, ID_B_instr_out, ID_29_a_out, ID_ALU_op3_out);
+
+    PipelineRegister_ID_EX PipelineRegister_ID_EX(Clk, PipelineRegister_IF_ID_Out, PipelineRegister_ID_EX_Out, ID_jmpl_instr_out, ID_Read_Write_out, ID_ALU_op3_out, ID_SE_dm_out, ID_load_instr_out, ID_RF_enable_out, ID_size_dm_out, ID_modifyCC_out, EX_jmpl_instr, EX_Read_Write, EX_ALU_op3, EX_SE_dm, EX_load_instr, EX_RF_enable, EX_size_dm, EX_modifyCC);
+
+    PipelineRegister_EX_MEM PipelineRegister_EX_MEM(Clk, PipelineRegister_ID_EX_Out, PipelineRegister_EX_MEM_Out, EX_jmpl_instr, EX_Read_Write, EX_SE_dm, EX_load_instr, EX_RF_enable, EX_size_dm, MEM_jmpl_instr, MEM_Read_Write, MEM_SE_dm, MEM_load_instr, MEM_RF_enable, MEM_size_dm);
+
+    PipelineRegister_MEM_WB PipelineRegister_MEM_WB(Clk, PipelineRegister_EX_MEM_Out, PipelineRegister_MEM_WB_Out, MEM_RF_enable, WB_RF_enable);
+
+    initial #52 $finish;
+
+    //Precargar file a Intruction Memory
+    initial begin
+        fr = $fopen("Fase3Memory.txt", "r");
+        Address = 8'b00000000;
+        while(!$feof(fr)) //fin del file
+            begin
+                code = $fscanf(fr, "%b", FileData); //leer del file un dato
+                InstructionMemory.Mem[Address] = FileData;
+                Address = Address + 1;
+            end
+            $fclose(fr); //cerrar file de lectura
+            Address = 8'b00000000; //make sure adress starts back in 0 after precharge
+    end
+
+    initial begin
+        Clk = 0; //La simulación debe comenzar inicializando Clk en cero a tiempo cero. Entonces, debe cambiar de estado cada dos unidades de tiempo de manera perpetua.
+        LE = 1'b1;
+        forever #2 Clk = ~Clk;
+    end
+
+    initial begin
+        R = 1; // La señal Reset debe tener un valor de 1 a tiempo cero y cambiar a 0 en tiempo 1. 
+        #1 R = 0;
+
+    end
+
+    initial begin
+        S = 0;  //La señal S del multiplexer debe tener un valor de cero a tiempo cero y debe cambiar a 1 a tiempo 40.
+        #40 S = ~S; 
+    end
+
+    initial begin
+        $monitor("Instruccion: %b PC: %d nPC: %d Clk: %b  Reset: %b  LE %b  S %b  Time: %d \n ID_op %b ID_ALU_op3 %b ID_jmpl_instr: %b , ID_Read_Write: %b , ID_SE_dm: %b , ID_load_instr: %b , ID_RF_enable: %b , ID_size_dm: %b , ID_modifyCC: %b , ID_Call_instr: %b , ID_B_instr: %b , ID_29_a: %b \n EX_jmpl_instr: %b, EX_ALU_op: %b , EX_Read_Write: %b, EX_SE_dm: %b , EX_load_instr: %b , EX_RF_enable: %b, EX_size_dm: %b , EX_modifyCC: %b \n MEM_jmpl_instr: %b , MEM_Read_Write: %b , MEM_SE_dm: %b , MEM_load_instr: %b , MEM_RF_enable: %b , MEM_size_dm: %b \n WB_RF_enable: %b", PipelineRegister_IF_ID_Out, PC_Out, PC_In, Clk, R, LE, S, $time, ID_op, ID_ALU_op3, ID_jmpl_instr_out, ID_Read_Write, ID_SE_dm, ID_load_instr, ID_RF_enable, ID_size_dm, ID_modifyCC, ID_Call_instr, ID_B_instr, ID_29_a, EX_jmpl_instr, EX_ALU_op3, EX_Read_Write, EX_SE_dm, EX_load_instr, EX_RF_enable, EX_size_dm, EX_modifyCC, MEM_jmpl_instr, MEM_Read_Write, MEM_SE_dm, MEM_load_instr, MEM_RF_enable, MEM_size_dm, WB_RF_enable);
+
+    end
+     
+endmodule
